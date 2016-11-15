fc81b1b2ebbd77cca3f76913a06fe44f6c96db5e
shamu92 <altshteyn@gmail.com>
2016-11-15 15:33:27 -0800

    got jobs from json

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Colonist, Job } from '../models';
+import JobsService from '../services/jobs.service'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers:[JobsService]
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  colonist: Colonist;
+  marsJobs: Job[];
+
+  constructor(jobService: JobsService) {
+    this.colonist = new Colonist('', null, null, null);
+    
+    jobService.getJobs().subscribe((jobs) => {
+      this.marsJobs = jobs;
+     
+    }, err => {
+      console.log(err);
+    });
+   }
 
   ngOnInit() {
   }
