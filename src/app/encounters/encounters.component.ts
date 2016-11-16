e87ab6600e6b136e09011dbe00f6ed9063fbdd37
shamu92 <altshteyn@gmail.com>
2016-11-15 18:57:28 -0800

    added encounters data from json

--- src/app/encounters/encounters.component.ts
+++ src/app/encounters/encounters.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Encounter } from '../models';
+import EncountersService from '../services/encounters.service'
+
 @Component({
   selector: 'app-encounters',
   templateUrl: './encounters.component.html',
-  styleUrls: ['./encounters.component.css']
+  styleUrls: ['./encounters.component.css'],
+  providers:[EncountersService]
 })
 export class EncountersComponent implements OnInit {
 
-  constructor() { }
+  marsEncounters: Encounter[];
+
+
+  constructor(encounterService: EncountersService) {
+    
+    encounterService.getEncounters().subscribe((encounters) => {
+      this.marsEncounters = encounters;
+     
+    }, err => {
+      console.log(err);
+    });
+   }
 
   ngOnInit() {
   }
+
+
 
 }
